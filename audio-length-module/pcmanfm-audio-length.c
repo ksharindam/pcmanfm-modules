f93cbd19828f8c843f1d5f85640d20dcffd7a1b7
Arindam Chaudhuri <ksharindam@gmail.com>
2019-07-30 11:05:04 +0530

    build dependency on libpoppler-glib-dev and libtagc-dev removed

--- audio-length-module/pcmanfm-audio-length.c
+++ audio-length-module/pcmanfm-audio-length.c
@@ -1,12 +1,42 @@
 #include <stdlib.h>
+#include <dlfcn.h>
 #include <libfm/fm.h>
 #include <pcmanfm-modules.h>
-#include <taglib/tag_c.h>
+//#include <taglib/tag_c.h>
 
 FM_DEFINE_MODULE(tab_page_status, audio_length)
 
+typedef void TagLib_File;
+typedef void TagLib_AudioProperties;
 
-static char *get_audio_len(FmFileInfoList *files, gint n_files){
+void *taglib;
+TagLib_File* (*taglib_file_new)(char*);
+int (*taglib_file_is_valid)(TagLib_File*);
+TagLib_AudioProperties* (*taglib_file_audioproperties)(TagLib_File*);
+int (*taglib_audioproperties_length)(TagLib_AudioProperties*);
+void (*taglib_tag_free_strings)();
+void (*taglib_file_free)(TagLib_File*);
+
+static gboolean _init_module()
+{
+    taglib = dlopen("libtag_c.so.0", RTLD_LAZY);
+    if (!taglib)
+        return 0;
+    taglib_file_new = dlsym(taglib, "taglib_file_new");
+    taglib_file_is_valid = dlsym(taglib, "taglib_file_is_valid");
+    taglib_file_audioproperties = dlsym(taglib, "taglib_file_audioproperties");
+    taglib_audioproperties_length = dlsym(taglib, "taglib_audioproperties_length");
+    taglib_tag_free_strings = dlsym(taglib, "taglib_tag_free_strings");
+    taglib_file_free = dlsym(taglib, "taglib_file_free");
+    return 1;
+}
+
+static void _finish_module()
+{
+    dlclose(taglib);
+}
+
+static char* _get_audio_len(FmFileInfoList *files, gint n_files){
 
     FmFileInfo *fi = fm_file_info_list_peek_head(files);
     if (n_files > 1 )
@@ -21,27 +51,26 @@ static char *get_audio_len(FmFileInfoList *files, gint n_files){
 	TL_file = taglib_file_new( filename );
 	free( filename );
 
-	if (TL_file != NULL) {
-		if(taglib_file_is_valid(TL_file)){
-			const TagLib_AudioProperties *TL_props = taglib_file_audioproperties( TL_file );
-			length = taglib_audioproperties_length( TL_props );
-			if( length>3600 ){
-				audio_len = g_strdup_printf( "  --> %d:%02d:%02d", length/3600, (length%3600)/60, length%60 );
-			}else{
-				audio_len = g_strdup_printf( "  --> %d:%02d", length/60, length%60 );
-			}
+	if ((TL_file == NULL) || (!taglib_file_is_valid(TL_file))) {
+        taglib_file_free( TL_file );
+        return NULL;
+    }
+    TagLib_AudioProperties *TL_props = taglib_file_audioproperties( TL_file );
+    length = taglib_audioproperties_length( TL_props );
+    if ( length>3600 ) {
+        audio_len = g_strdup_printf( "  --> %d:%02d:%02d", length/3600, (length%3600)/60, length%60 );
+    } else {
+        audio_len = g_strdup_printf( "  --> %d:%02d", length/60, length%60 );
+    }
 
-			taglib_tag_free_strings();
-			taglib_file_free( TL_file );
-			return audio_len;
-		}
-	}
-	return NULL;
+    taglib_tag_free_strings();
+    taglib_file_free( TL_file );
+    return audio_len;
 };
 
 
 FmTabPageStatusInit fm_module_init_tab_page_status = {
-  NULL,
-  NULL,
-  .sel_message = get_audio_len
+    _init_module,
+    _finish_module,
+    _get_audio_len
 };
--- pdf-pages-module/pcmanfm-pdf-pages.c
+++ pdf-pages-module/pcmanfm-pdf-pages.c
@@ -1,9 +1,31 @@
 #include <stdlib.h>
-#include <string.h>
+#include <dlfcn.h>
 #include <pcmanfm-modules.h>
-#include <poppler/glib/poppler-document.h>
+//#include <poppler/glib/poppler-document.h>
+
+typedef void PopplerDocument;
+
+void* poppler;
+PopplerDocument* (*poppler_document_new_from_file)(char*, char*/*passwd*/, GError**);
+int (*poppler_document_get_n_pages)(PopplerDocument*);
+
 
 FM_DEFINE_MODULE( tab_page_status, pdf_pages )
+
+static gboolean _init_module()
+{
+    poppler = dlopen("libpoppler-glib.so.8", RTLD_LAZY);
+    if (!poppler)
+        return 0;
+    poppler_document_new_from_file = dlsym(poppler, "poppler_document_new_from_file");
+    poppler_document_get_n_pages = dlsym(poppler, "poppler_document_get_n_pages");
+    return 1;
+}
+
+static void _finish_module()
+{
+    dlclose(poppler);
+}
 
 static char *_sel_message( FmFileInfoList *files, gint n_files )
 {
@@ -12,7 +34,7 @@ static char *_sel_message( FmFileInfoList *files, gint n_files )
 
 	FmFileInfo *fi = fm_file_info_list_peek_head( files );
 	const char* mimetype = fm_mime_type_get_type( fm_file_info_get_mime_type( fi ) );
-	if( strcmp( mimetype, "application/pdf" ) != 0 ){
+	if ( strcmp( mimetype, "application/pdf" ) != 0 ){
 		return 0;
 	}
 
@@ -23,26 +45,26 @@ static char *_sel_message( FmFileInfoList *files, gint n_files )
 
 	filename = fm_path_to_uri( fm_file_info_get_path( fi ) );
 	document = poppler_document_new_from_file( filename, NULL, &error );
-	if( error != NULL )
+	if (error != NULL)
 	{
 		fprintf (stderr, "statusbar-pdf-pages error: %s\n", error->message);
 		g_error_free( error );
 	}
 	free( filename );
-	if ( document == NULL ){
+	if (document == NULL) {
 		return 0;
 	}
 	num_pages = poppler_document_get_n_pages( document );
 	g_object_unref(document);
 
-	if( num_pages == 1 ){
-		return " ( 1 page )";
+	if (num_pages == 1) {
+		return g_strdup(" ( 1 page )");
 	}
-    return g_strdup_printf( " ( %i pages )", num_pages);
+    return g_strdup_printf(" ( %i pages )", num_pages);
 }
 
 FmTabPageStatusInit fm_module_init_tab_page_status = {
-    NULL,
-    NULL,
+    _init_module,
+    _finish_module,
     _sel_message
 };
